fa7528df1d56964cf1ee81250e82d7d01a87501e
Jaroslav Brtiš <jaroslav.brtis@gmail.com>
2017-11-01 23:43:06 +0100

    Added EnumLabelConverter.

---
+++ src/main/java/com/jardoapps/zk/utils/converters/EnumLabelConverter.java
@@ -0,0 +1,21 @@
+package com.jardoapps.zk.utils.converters;
+
+import org.zkoss.bind.BindContext;
+import org.zkoss.bind.Converter;
+import org.zkoss.util.resource.Labels;
+import org.zkoss.zk.ui.Component;
+
+public class EnumLabelConverter implements Converter<String, Enum<?>, Component> {
+
+	@Override
+	public String coerceToUi(Enum<?> beanProp, Component component, BindContext ctx) {
+		String key = String.format("%s.%s", beanProp.getClass().getSimpleName(), beanProp.name());
+		return Labels.getLabel(key, beanProp.name());
+	}
+
+	@Override
+	public Enum<?> coerceToBean(String compAttr, Component component, BindContext ctx) {
+		throw new UnsupportedOperationException("Not implemented");
+	}
+
+}

efd66dcb90ccf2c080d22bef6cbdd808f5fb4d51
Jaroslav Brtiš <jaroslav.brtis@gmail.com>
2017-11-01 23:50:36 +0100

    Added LocalDateConverter.

---
+++ src/main/java/com/jardoapps/zk/utils/converters/LocalDateConverter.java
@@ -0,0 +1,24 @@
+package com.jardoapps.zk.utils.converters;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+import org.zkoss.bind.BindContext;
+import org.zkoss.bind.Converter;
+import org.zkoss.zk.ui.Component;
+
+public class LocalDateConverter implements Converter<Date, LocalDate, Component> {
+
+	@Override
+	public Date coerceToUi(LocalDate beanProp, Component component, BindContext ctx) {
+		return Date.from(beanProp.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
+	}
+
+	@Override
+	public LocalDate coerceToBean(Date compAttr, Component component, BindContext ctx) {
+		return Instant.ofEpochMilli(compAttr.getTime()).atZone(ZoneId.systemDefault()).toLocalDate();
+	}
+
+}
